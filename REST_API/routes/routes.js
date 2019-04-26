8868e3c1e71cb92ef8da91588aae1bc15837be12
Aleem Khan <aleemkirk@gmail.com>
2019-04-26 11:43:13 -0400

    added rest API

--- REST_API/routes/routes.js
+++ REST_API/routes/routes.js
@@ -11,57 +11,65 @@ var connection = mysql.createConnection({
 
 var appRouter = function(app){
 
+    //-->Get full list of player names from 'players' table
     app.get("/players", function(req, res){
         connection.query('SELECT `name` from `players`', function(err, rows, fields){
             if(err) throw err;
-            res.status(200).send(JSON.stringify({player_names: rows}));
+            res.status(200).send(JSON.stringify(rows));
         });
     });
 
-    app.get("/player/:num", function(req, res){
-        var index = req.params.num;
-        if(isFinite(index) && index >=0){
-            connection.query('SELECT * FROM `players` ORDER BY `id` LIMIT 1 OFFSET '+ index +';', function(err, row, fields){
-                if(err) throw err;
-                res.status(200).send(JSON.stringify({player: row}))
+
+    //-->Get player general information from 'players' table by using 'id' 
+    app.get("/player/:info", function(req, res){
+        var re = /^[A-Za-z_A-Za-z]+$/;
+        var info = req.params.info;
+        if(isFinite(info) && info >=0){ 
+            connection.query('SELECT * FROM `players` ORDER BY `id` LIMIT 1 OFFSET '+ info +';', function(err, row, fields){
+                if(err) {res.status(500).send({error: err});}
+                else{res.status(200).send(JSON.stringify(row));}
 
             });
-        }else{res.status(404).send({'error message': "enter valid number"});}
+        }else if(re.test(info)){
+            connection.query("SELECT * FROM `players` WHERE (`Name` = '" + info.replace('_', ' ') + "');", function(err, row, fields){
+                if(err) {res.status(500).send({error: err});}
+                else{res.status(200).send(JSON.stringify(row));}
+            });
+        }else{res.status(404).send({'error message': "enter valid player info"});}
 
     });
 
-    
+    //-->Get player match stats using player name
+    app.get("/match_stats/:player_name", function(req, res){
+        var re = /^[A-Za-z_A-Za-z]+$/;
+        var name = req.params.player_name;
+        if(re.test(name)){
+            connection.query('SELECT * FROM '+name+';', function(err, table, fields){
+                if(err){res.status(500).send({error: err});}
+                else{res.status(200).send(JSON.stringify(table));}
+            });
+        }else{
+            res.status(400).send({error:'invalid name'})
+        }
+    });
 
-    // app.get("/user", function(req, res){
-    //     var data = ({
-    //         firstName: faker.name.firstName(),
-    //         lastName: faker.name.lastName(),
-    //         userName: faker.internet.userName(),
-    //         email: faker.internet.email()
-    //     });
-    //     res.status(200).send(data);
-    // });
+    //-->API for search functionality
+    app.get("/player_search/:player_name", function(req, res){
+        var re = /^[A-Za-z_A-Za-z]+$/;
+        var name = req.params.player_name;
+        const limit = 10; //how much result are returned
+        if(re.test(name)){
+            connection.query('SELECT * FROM `players` WHERE `Name` LIKE \'%'+name.replace('_', ' ')+'%\' LIMIT ' + limit +';', function(err, rows, fields){
+                if(err){res.status(500).send({error:err})}
+                else{
+                    res.status(200).send(JSON.stringify(rows));
+                }
+            });
+        }else{
+            res.status(400).send({error:"invalid player name"})
+        }
+    });
 
-    // app.get("/user/:num", function(req, res){
-    //     var users = [];
-    //     var num = req.params.num;
-
-    //     if(isFinite(num) && num > 0){
-    //         for(i =0; i <= num-1; i++){
-    //             users.push({
-    //                 firstName: faker.name.firstName(),
-    //                 lastName: faker.name.lastName(),
-    //                 userName: faker.internet.userName(),
-    //                 email: faker.internet.email()
-    //             });
-    //         }
-            
-    //         res.status(200).send({info: users});
-
-    //     } else{
-    //         res.status(400).send({message: 'invalid number supplied'});
-    //     }
-    // });
 
 }
 
